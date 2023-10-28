79ef822254afbb795abc97805761095e91e3ceff
stewarteaimee <145696005+stewarteaimee@users.noreply.github.com>
2023-10-28 18:28:45 -0400

    Samedi tunning

--- src/sources/main.cpp
+++ src/sources/main.cpp
@@ -29,7 +29,7 @@ void loop(void) {
     c = tcs.read16(TCS34725_CDATAL);
 
     tcs.getRawData(&r, &g, &b, &c);
-/*
+
     Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
     Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
     Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
@@ -39,34 +39,42 @@ void loop(void) {
   
        // Add a delay to control the update rate (in milliseconds)
     delay(100);
-  */
+
+ //These values work when strapped directly to the motor'
   
-  if (r > 70 && r < 90 && g > 65 && g < 75 && b > 65 && b < 80 && c > 230 && c < 250){
+  if (r > 90 && r < 155 && g > 60 && g < 100 && b > 50 && b < 100 && c > 250 && c < 360){
     Serial.println("Red");
   }
-  else if (r > 145 && r < 160 && g > 140 && g < 150 && b > 85 && b < 105 && c > 400 && c < 445)
+  else if (r > 200 && r < 330 && g > 200 && g < 300 && b > 100 && b < 200 && c > 500 && c < 900)
   {
     Serial.println("Yellow");
   }
-  else if (r > 40 && r < 50 && g > 70 && g < 80 && b > 70 && b < 90 && c > 200 && c < 240)
+   
+  else if (r > 45 && r < 70 && g > 90 && g < 120 && b > 110 && b < 170 && c > 250 && c < 350)
   {
     Serial.println("Blue");
   }
-  
-  else if (r > 45 && r < 60 && g > 75 && g < 90 && b > 65 && b < 90 && c > 220 && c < 245)
+  else if (r > 50 && r < 69 && g > 95 && g < 150 && b > 80 && b < 130 && c > 250 && c < 400)
   {
     Serial.println("Green");
   }
-  
-  else if (r > 175 && r < 190 && g > 185 && g < 200 && b > 165 && b < 180 && c > 540 && c < 590)
-  {
-    Serial.println("White");
-  }
-
-  else if (r > 55 && r < 70 && g > 75 && g < 85 && b > 75 && b < 85 && c > 225 && c < 255)
+ else if (r > 70 && r < 95 && g > 95 && g < 200 && b > 100 && b < 200 && c > 275 && c < 350)
   {
     Serial.println("Carpet");
   }
-  
+
+  else if (r > 50 && r < 110 && g > 105 && g < 120 && b > 70 && b < 90 && c > 300 && c < 350)
+  {
+    Serial.println("Black");
+  } 
+
+  else if (r > 350 && r < 400 && g > 400 && g < 450 && b > 350 && b < 500 && c > 1000 && c < 1500)
+  {
+    Serial.println("White");
+  }
+  /*else{
+    Serial.println("IDK");
+  }*/
 
 }
+//good one
