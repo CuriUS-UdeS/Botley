2ad6b70045a34299662581c1feb45b9805a44a7f
Charles-cad <charlesarieldion@gmaIl.com>
2023-11-01 18:49:29 -0400

    ajout calibration couleur verte et jaune

--- src/sources/main.cpp
+++ src/sources/main.cpp
@@ -13,39 +13,39 @@ unsigned int sensorValues[NUM_SENSORS];
 
 //@brief suiveurDeLigne : Tous les variables globales pour la fonction de PID qui suit ligne (PIDLigne)
 struct suiveurDeLigne{
-//@brief error : a quelle point on est off de la ligne
-//@brief adjustement : correction a ajouter a la vitesse du moteur
-//@brief lastError : memorisation pour le calcul de la derivée 
+  //@brief error : a quelle point on est off de la ligne
+  //@brief adjustement : correction a ajouter a la vitesse du moteur
+  //@brief lastError : memorisation pour le calcul de la derivée 
 
-// PID Properties
-const double KP   = 0.00008;
-const double KI   = 0.00003;
-const double KD   = 0.12  ; //mis haut pour avoir reactivite beaucoup trop intense pour faire des tournants incroyable youpi
+  // PID Properties
+  const double KP   = 0.00008;
+  const double KI   = 0.00003;
+  const double KD   = 0.12  ; //mis haut pour avoir reactivite beaucoup trop intense pour faire des tournants incroyable youpi
 
-//init des variable pour le calcul de l'erreur
-float error       = 0;
-float adjustment  = 0;
+  //init des variable pour le calcul de l'erreur
+  float error       = 0;
+  float adjustment  = 0;
 
-//derivate
-double lastError  = 0.0;
+  //derivate
+  double lastError  = 0.0;
 
-//integral
-double integral   = 0.0;
-float integralMin = -10000.0; //juste pour eviter que lintegrale depasse
-float integralMax = 10000.0; //juste pour eviter que lintegrale depasse
+  //integral
+  double integral   = 0.0;
+  float integralMin = -10000.0; //juste pour eviter que lintegrale depasse
+  float integralMax = 10000.0; //juste pour eviter que lintegrale depasse
 
-const float GOAL = 3500; //au millieu du board capteur 4 & 5, lit dequoi entre 0 et 7000 nous on veut [etre au centre donc 3500
-const double MAX_SPEED = 0.5;
-const double MIN_SPEED = 0.0;
+  const float GOAL = 3500; //au millieu du board capteur 4 & 5, lit dequoi entre 0 et 7000 nous on veut [etre au centre donc 3500
+  const double MAX_SPEED = 0.5;
+  const double MIN_SPEED = 0.0;
 
-//position de la ligne
-int position; //valeur entre 0 et 7000
+  //position de la ligne
+  int position; //valeur entre 0 et 7000
 
-//valeurs pour fonction de calibration
-const int Manuel       = 0; //= 0 met les valeurs pour le defi du combattant dans la calibration
-const int Automatique  = 1; //= 1 call fonction pour trouver automatiquement des valeurs min et max  pour le capteur
-const int Raw          = 2; //= 2 call fonction pour le debug et test de fonctionnement des capteurs
-};
+  //valeurs pour fonction de calibration
+  const int Manuel       = 0; //= 0 met les valeurs pour le defi du combattant dans la calibration
+  const int Automatique  = 1; //= 1 call fonction pour trouver automatiquement des valeurs min et max  pour le capteur
+  const int Raw          = 2; //= 2 call fonction pour le debug et test de fonctionnement des capteurs
+  };
 suiveurDeLigne FollowLine;
 
 //moteurs
@@ -92,11 +92,58 @@ void AutomaticCalibrateLineSensor(){
 
 }
 
-//@brief ManualCalibrateLineSensor() : Fonction pour mettre directement les valeurs pour le defi du parcours dans la calibration du capteur
-void ManualCalibrateLineSensor(){
+
+//@brief ManualCalibrateLineSensor() : WHITE Fonction pour mettre directement les valeurs pour le defi du parcours dans la calibration du capteur
+void ManualCalibrateLineSensorWhite(){
   //valeurs min et max pour les capteurs 
   unsigned int minValues[NUM_SENSORS] = {33, 32, 31, 33, 30, 28, 30, 27,};
-  unsigned int maxValues[NUM_SENSORS] = {880, 701, 745, 743, 713, 798, 693, 837}; 
+  unsigned int maxValues[NUM_SENSORS] = {880, 701, 745, 743, 713, 798, 693, 837};
+  
+  //initialise les valeurs de la librairie 
+  qtra.calibrate(); 
+
+  //ecrit manuellement toute les valeurs min des capteurs pour le calcul de la position de la  ligne
+  for (int i = 0; i < NUM_SENSORS; i++)
+  {
+    qtra.calibratedMinimumOn[i] = minValues[i];
+  }
+  
+  //ecrit manuellement toute les valeurs max des capteurs pour le calcul de la position de la  ligne
+  for (int j = 0; j < NUM_SENSORS; j++)
+  {
+    qtra.calibratedMaximumOn[j] = maxValues[j];
+  }
+
+}
+
+//@brief ManualCalibrateLineSensor() : GREEN Fonction pour mettre directement les valeurs pour le defi du parcours dans la calibration du capteur
+void ManualCalibrateLineSensorGreen(){
+  //valeurs min et max pour les capteurs 
+  unsigned int minValues[NUM_SENSORS] = {150, 43, 47, 90, 41, 35, 37, 34};
+  unsigned int maxValues[NUM_SENSORS] = {940, 721, 841, 904, 882, 760, 804, 767};
+  
+  //initialise les valeurs de la librairie 
+  qtra.calibrate(); 
+
+  //ecrit manuellement toute les valeurs min des capteurs pour le calcul de la position de la  ligne
+  for (int i = 0; i < NUM_SENSORS; i++)
+  {
+    qtra.calibratedMinimumOn[i] = minValues[i];
+  }
+  
+  //ecrit manuellement toute les valeurs max des capteurs pour le calcul de la position de la  ligne
+  for (int j = 0; j < NUM_SENSORS; j++)
+  {
+    qtra.calibratedMaximumOn[j] = maxValues[j];
+  }
+
+}
+
+//@brief ManualCalibrateLineSensor() : YELLOW Fonction pour mettre directement les valeurs pour le defi du parcours dans la calibration du capteur
+void ManualCalibrateLineSensorYellow(){
+  //valeurs min et max pour les capteurs 
+  unsigned int minValues[NUM_SENSORS] = {41, 38, 38, 42, 35, 30, 33, 30};
+  unsigned int maxValues[NUM_SENSORS] = {885, 676, 859, 907, 864, 822, 460, 724};
   
   //initialise les valeurs de la librairie 
   qtra.calibrate(); 
@@ -140,10 +187,15 @@ void litAnalogIn(){
 }
 
 //@brief Calibrate_Line_Sensor : Manuel -> AutoMan = 0 ------ Automatique -> AutoMan = 1 ------- Raw -> AutoMan = 2
-void Calibrate_Line_Sensor(int AutoMan) { // Automatique = 1 Manuel = 0 (pour le tuning pour le defi du combattant prendre Manuel)
+void Calibrate_Line_Sensor(int AutoMan, int color) { // Automatique = 1 Manuel = 0 (pour le tuning pour le defi du combattant prendre Manuel)
   
   if (AutoMan == 0){
-    ManualCalibrateLineSensor();
+    if (color == 0)
+      ManualCalibrateLineSensorWhite();
+    if (color == 1)
+      ManualCalibrateLineSensorGreen();
+    if (color == 2)
+      ManualCalibrateLineSensorYellow();
   }
   if (AutoMan == 1){   
     AutomaticCalibrateLineSensor();
@@ -210,7 +262,7 @@ void setup() {
   MOTOR_SetSpeed(droite, 0);
  
  //Calibre le capteur de suiveur de ligne 
-  Calibrate_Line_Sensor(FollowLine.Manuel);
+      //Calibrate_Line_Sensor(FollowLine.Manuel, 0);
   
 }
 
@@ -218,9 +270,57 @@ void setup() {
 void loop() {
 
 //condition pour comencer le suiveur de ligne ex: la couleur
-    //PIDLigne();
+//PIDLigne();
+int test = 0;
+
+Serial.print("Testing line on white: ");
+Calibrate_Line_Sensor(FollowLine.Manuel, 1);
+delay (1000);
+
+for (int i = 0; i<400; i++){  
+  test = ImIOnLine ();
+  if (test == 1) {
+      Serial.println ("im on the line!");
+    }
+    else {
+      Serial.println ("im not on the line");
+    }
+}
+
+Serial.println("white testing finished ");
+Serial.println();
+delay (5000);
 
 
+Serial.println("testing line on Green: ");
+Calibrate_Line_Sensor(FollowLine.Manuel, 1);
+for (int i = 0; i<400; i++){  
+  test = ImIOnLine ();
+  if (test == 1) {
+      Serial.println ("im on the line!");
+    }
+    else {
+      Serial.println ("im not on the line");
+    }
+}
+Serial.println();
+Serial.println("green testing finished ");
+
+delay(5000);
+Serial.print("testing line on Yellow: ");
+Calibrate_Line_Sensor(FollowLine.Manuel, 2);
+for (int i = 0; i<400; i++){  
+  test = ImIOnLine ();
+  if (test == 1) {
+      Serial.println ("im on the line!");
+    }
+    else {
+      Serial.println ("im not on the line");
+    }
+}
+Serial.println("yellow testing finished ");
+
+/*
 //test fonction 
 int test = ImIOnLine ();
 
@@ -234,4 +334,6 @@ int test = ImIOnLine ();
     Serial.println(valuesrandom);
       
 }
-   
+
+*/
+}
