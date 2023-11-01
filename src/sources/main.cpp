9e5dc174f435efb99a4a33a0bbeb9776b67fe4c8
Charles-cad <charlesarieldion@gmaIl.com>
2023-10-31 20:07:17 -0400

    conmentaire et ready to merge

--- src/sources/main.cpp
+++ src/sources/main.cpp
@@ -11,129 +11,51 @@
 QTRSensorsAnalog qtra((unsigned char[]) {A1, A2, A4, A5, A6, A7, A8, A9}, NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
 unsigned int sensorValues[NUM_SENSORS];
 
+//@brief suiveurDeLigne : Tous les variables globales pour la fonction de PID qui suit ligne (PIDLigne)
+struct suiveurDeLigne{
+//@brief error : a quelle point on est off de la ligne
+//@brief adjustement : correction a ajouter a la vitesse du moteur
+//@brief lastError : memorisation pour le calcul de la derivée 
 
 // PID Properties
 const double KP   = 0.00008;
 const double KI   = 0.00003;
-const double KD   = 0.12  ; //0.000001;
+const double KD   = 0.12  ; //mis haut pour avoir reactivite beaucoup trop intense pour faire des tournants incroyable youpi
 
-float error      = 0;
+//init des variable pour le calcul de l'erreur
+float error       = 0;
 float adjustment  = 0;
+
 //derivate
 double lastError  = 0.0;
+
 //integral
 double integral   = 0.0;
-float integralMin = -10000.0;
-float integralMax = 10000.0;
+float integralMin = -10000.0; //juste pour eviter que lintegrale depasse
+float integralMax = 10000.0; //juste pour eviter que lintegrale depasse
 
 const float GOAL = 3500; //au millieu du board capteur 4 & 5, lit dequoi entre 0 et 7000 nous on veut [etre au centre donc 3500
 const double MAX_SPEED = 0.5;
 const double MIN_SPEED = 0.0;
 
+//position de la ligne
+int position; //valeur entre 0 et 7000
+
+//valeurs pour fonction de calibration
+const int Manuel       = 0; //= 0 met les valeurs pour le defi du combattant dans la calibration
+const int Automatique  = 1; //= 1 call fonction pour trouver automatiquement des valeurs min et max  pour le capteur
+const int Raw          = 2; //= 2 call fonction pour le debug et test de fonctionnement des capteurs
+};
+suiveurDeLigne FollowLine;
 
 //moteurs
 const int gauche = 0;
 const int droite = 1;
 
-/*
-int detectNoir(int InputCapteur){
-  //fonction pour savoir si un capteur du suiveur de ligne voit du noir
-  float valeurMax = 750;
-  float valeurMin = 550;
-  
-  if (valeurMin <= InputCapteur <= valeurMax){
-    return 1;
-  }
-  else{
-    return 0;
-  }
-
-return 0;
-}
-
-int Suiveur_Ligne (){
-  float OUT = 0.0;
-  int valeurSuiveurLigne1 = analogRead (SuiveurLigne1);
-  int valeurSuiveurLigne2 = analogRead (SuiveurLigne2);
-  int valeurSuiveurLigne3 = analogRead (SuiveurLigne3);
-  int valeurSuiveurLigne4 = analogRead (SuiveurLigne4);
-  int valeurSuiveurLigne5 = analogRead (SuiveurLigne5);
-  int valeurSuiveurLigne6 = analogRead (SuiveurLigne6);
-  int valeurSuiveurLigne7 = analogRead (SuiveurLigne7);
-  int valeurSuiveurLigne8 = analogRead (SuiveurLigne8);
-
-  int S1 = detectNoir(valeurSuiveurLigne1);
-  int S2 = detectNoir(valeurSuiveurLigne2);
-  int S3 = detectNoir(valeurSuiveurLigne3);
-  int S4 = detectNoir(valeurSuiveurLigne4);
-  int S5 = detectNoir(valeurSuiveurLigne5);
-  int S6 = detectNoir(valeurSuiveurLigne6);
-  int S7 = detectNoir(valeurSuiveurLigne7);
-  int S8 = detectNoir(valeurSuiveurLigne8);
-
-  //Trouve la position des bits ex: 1 0 0 0 0 0 0 0 ou 1 1 0 0 0 0 0 0
-  int position8bits[] = {0, 0, 0 , 0 , 0 , 0 , 0 , 0}; 
-  if (S8 = 1){
-    position8bits[0] = 1;
-  } 
-  if (S7 = 1){
-    position8bits[1] = 1;
-  }
-  if (S6 = 1){
-    position8bits[2] = 1;
-  } 
-  if (S5 = 1){
-    position8bits[3] = 1;
-  } 
-  if (S4 = 1){
-    position8bits[4] = 1;
-  }
-  if (S3 = 1){
-    position8bits[5] = 1;
-  } 
-  if (S2 = 1){
-    position8bits[6] = 1;
-  } 
-  if (S1 = 1){
-    position8bits[7] = 1;
-  } 
-
-  //si la valeur est bizzard (ex il detect deux lignes)
-  if ((position8bits != 2^0) &&  (position8bits != 2^0 + 2^1) &&
-      (position8bits != 2^1) &&  (position8bits != 2^2 + 2^1) &&
-      (position8bits != 2^2) &&  (position8bits != 2^3 + 2^2) &&
-      (position8bits != 2^3) &&  (position8bits != 2^4 + 2^3) &&
-      (position8bits != 2^4) &&  (position8bits != 2^5 + 2^4) &&
-      (position8bits != 2^5) &&  (position8bits != 2^6 + 2^5) &&
-      (position8bits != 2^6) &&  (position8bits != 2^7 + 2^6) &&
-      (position8bits != 2^7)
-    )
-  {
-    OUT = 99;
-  }
-  
-
-
-return OUT;
-}
-*/
-
-void calibrateLineSensor(){
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);    // turn on LED to indicate we are in calibration mode
-  for (int i = 0; i < 400; i++)  // make the calibration take about 10 seconds
-  {
-    qtra.calibrate();       // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
-  }
-  digitalWrite(13, LOW);     // turn off  LED to indicate we are through with calibration
-
-  for (int i = 0; i < 400; i++)  // make the calibration take about 10 seconds
-  {
-    qtra.calibrate();       // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
-  }
-
-//reste a effacer et hard coder les valeurs min et max de detection
+//@brief displayValues : Fonction pour afficher les valeurs du capteur obtenue dans la calibration
+void displayValues(){
   // print the calibration minimum values measured when emitters were on
+  Serial.print("   display calib   ");
   Serial.begin(9600);
   for (int i = 0; i < NUM_SENSORS; i++)
   {
@@ -151,42 +73,49 @@ void calibrateLineSensor(){
   Serial.println();
 }
 
-void manualCalibrateLineSensor(){
-  Serial.print("   manuel calib   ");
+//@brief AutomaticCalibrateLineSensorln : Fonction pour trouver, sauvegarder et imprimer les valeurs min et max des capteurs de lignes
+void AutomaticCalibrateLineSensor(){
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);    // turn on LED to indicate we are in calibration mode
+  for (int i = 0; i < 400; i++)  // make the calibration take about 10 seconds
+  {
+    qtra.calibrate();       // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
+  }
+  digitalWrite(13, LOW);     // turn off  LED to indicate we are through with calibration
+
+  for (int i = 0; i < 400; i++)  // make the calibration take about 10 seconds
+  {
+    qtra.calibrate();       // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
+  }
+
+//reste a effacer et hard coder les valeurs min et max de detection
+
+}
+
+//@brief ManualCalibrateLineSensor() : Fonction pour mettre directement les valeurs pour le defi du parcours dans la calibration du capteur
+void ManualCalibrateLineSensor(){
+  //valeurs min et max pour les capteurs 
   unsigned int minValues[NUM_SENSORS] = {33, 32, 31, 33, 30, 28, 30, 27,};
   unsigned int maxValues[NUM_SENSORS] = {880, 701, 745, 743, 713, 798, 693, 837}; 
   
+  //initialise les valeurs de la librairie 
   qtra.calibrate(); 
 
+  //ecrit manuellement toute les valeurs min des capteurs pour le calcul de la position de la  ligne
   for (int i = 0; i < NUM_SENSORS; i++)
   {
-    //Serial.print(i);
     qtra.calibratedMinimumOn[i] = minValues[i];
-   //Serial.print("   finforloop1   ");
   }
   
+  //ecrit manuellement toute les valeurs max des capteurs pour le calcul de la position de la  ligne
   for (int j = 0; j < NUM_SENSORS; j++)
   {
-    
-    //Serial.print(j);
     qtra.calibratedMaximumOn[j] = maxValues[j];
-    //Serial.print("   finforloop2   ");
   }
 
 }
 
-
-
-void displayValues(){
-  Serial.print("   display calib   ");
-  for (int i = 0; i < NUM_SENSORS; i++){
-    Serial.print(qtra.calibratedMaximumOn[i]);
-    Serial.println();
-    Serial.print(qtra.calibratedMinimumOn[i]);
-    Serial.println();
-  }
-}
-
+//@brief litAnalogIn : fonction debug pour lire et imprimer les valeurs brutes des capteurs 
 void litAnalogIn(){
   //fonction pour tester les valeurs 
   int A = analogRead(A1);
@@ -210,91 +139,99 @@ void litAnalogIn(){
 
 }
 
+//@brief Calibrate_Line_Sensor : Manuel -> AutoMan = 0 ------ Automatique -> AutoMan = 1 ------- Raw -> AutoMan = 2
+void Calibrate_Line_Sensor(int AutoMan) { // Automatique = 1 Manuel = 0 (pour le tuning pour le defi du combattant prendre Manuel)
+  
+  if (AutoMan == 0){
+    ManualCalibrateLineSensor();
+  }
+  if (AutoMan == 1){   
+    AutomaticCalibrateLineSensor();
+  }
+  if (AutoMan == 2){
+    litAnalogIn();
+  }
+  Serial.println("Calition for the line follower finished ");
+return;
+}
+
+//@brief ImIOnLine : fonction qui detect s'il est sur une bonne 
+int ImIOnLine(){
+  int line = qtra.readLine(sensorValues);
+
+  if (0 < line && line < 7000){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
 void PIDLigne(){
+  //lire capteur
+  FollowLine.position = qtra.readLine(sensorValues); //lit dequoi entre 0 et 7000 nous on veut [etre au centre donc 3500
+  
+  //calculate error
+  FollowLine.error = FollowLine.GOAL - FollowLine.position;
+  
   //acumulate error if not in weird state
+  if (abs(FollowLine.error) > 3000){
+    FollowLine.integral = 0.0;
+  }
+  else if (abs(FollowLine.error) < 3000){
+    FollowLine.integral += FollowLine.error;
+  }
 
-  if (abs(error) > 3000){
-    integral = 0.0;
-  }
-  else if (abs(error) < 3000){
-    integral += error;
-  }
   //Constrait integral
-  integral = constrain (integral, integralMin, integralMax);
+  FollowLine.integral = constrain (FollowLine.integral, FollowLine.integralMin, FollowLine.integralMax);
 
-
-  //calculate adjustment (new motor speed)
-  adjustment = KP*error + KI*(integral) + KD*(error - lastError);
+  //calculate adjustment to motor avec PID
+  FollowLine.adjustment = FollowLine.KP*FollowLine.error + FollowLine.KI*(FollowLine.integral) + FollowLine.KD*(FollowLine.error - FollowLine.lastError);
   
   // Store error for next increment
-  lastError = error;
+  FollowLine.lastError = FollowLine.error;
 
-  //MOTOR_SetSpeed(gauche, 0.4);
-  //MOTOR_SetSpeed(droite, 0.4);
-  //set les vitesses du moteur
-  MOTOR_SetSpeed(gauche, (constrain(MAX_SPEED - adjustment, MIN_SPEED, MAX_SPEED)));
-  MOTOR_SetSpeed(droite, (constrain(MAX_SPEED + adjustment, MIN_SPEED, MAX_SPEED)));
-
-
+  //set les nouvelles vitesses du moteur
+  MOTOR_SetSpeed(gauche, (constrain(FollowLine.MAX_SPEED - FollowLine.adjustment, FollowLine.MIN_SPEED, FollowLine.MAX_SPEED)));
+  MOTOR_SetSpeed(droite, (constrain(FollowLine.MAX_SPEED + FollowLine.adjustment, FollowLine.MIN_SPEED, FollowLine.MAX_SPEED)));
 }
+
 void setup() {
+  //Beginning
   Serial.begin(9600);
-  Serial.print("allo je commence");
+  Serial.print("Début du setup");
  
+ //init arduino X
   BoardInit();
   Serial.println(" Board init finit");
+
+  //S'assure que les motuers sont a 0
   MOTOR_SetSpeed(gauche, 0);
   MOTOR_SetSpeed(droite, 0);
  
- //Calibration du capteur
-  //calibrateLineSensor(); Serial.println("Displaying values"); displayValues(); //Automatique
-
-  manualCalibrateLineSensor();        //Manuel
-  Serial.println("Calibrate finit");  //Debug
+ //Calibre le capteur de suiveur de ligne 
+  Calibrate_Line_Sensor(FollowLine.Manuel);
   
-  //displayValues();
 }
 
+
 void loop() {
-  // Get line position
-  //litAnalogIn();
-  
-  //uncomment
-    int position = qtra.readLine(sensorValues); //lit dequoi entre 0 et 7000 nous on veut [etre au centre donc 3500
-      //Serial.print("position: ");
-      //Serial.println(position);
-    // Compute error
-    error = GOAL - position;
-    PIDLigne();
-    Serial.println(error);
-  //CODE POUR PID
 
-  // Compute motor adjustment
- /* 
-  if (position == 7000){
-    MOTOR_SetSpeed(gauche, 0.1);
-    MOTOR_SetSpeed(droite, 0.2);
-  }
-  else if (position == 0){
-    MOTOR_SetSpeed(gauche, 0.2);
-    MOTOR_SetSpeed(droite, 0.1);
-  }
-  else  if(position > 0 && position < 7000) {
-    
-
-  }
-*/
-  
-  
+//condition pour comencer le suiveur de ligne ex: la couleur
+    //PIDLigne();
 
 
-  /*
-  Serial.print("Integral: ");
-  Serial.println(integral);
-  Serial.print("Error: ");
-  Serial.println(error);
-  Serial.print("Adjustment: ");
-  Serial.println(adjustment);
-  */
+//test fonction 
+int test = ImIOnLine ();
 
-  }
+    if (test == 1) {
+      Serial.println ("im on the line!") ;
+    }
+    else {
+      Serial.println ("im not on the line");
+    }
+    int valuesrandom= qtra.readLine(sensorValues);
+    Serial.println(valuesrandom);
+      
+}
+   
